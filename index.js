a934452c363f9514960c179d9893a439fa23daf8
Agamenon <agamenonnas@gmail.com>
2023-08-16 03:14:49 -0300

    initil comit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");//Importando epress
+const app = express();//inicalizando  o express
+
+app.get("/", function(req, res){
+  res.send("Bem vindo ao Guia do Programador");
+});
+
+app.get("/Blog", function(req, res){
+  res.send("Bem vindo ao Meu Blog");
+});
+
+app.get("/Canal", function(req, res){
+  res.send("Bem vindo ao Meu Canal");
+});
+
+
+
+app.listen(4000, function (erro) {
+  if(erro){
+    console.log("Aconteceu um erro!");
+  }else{
+    console.log("Servidor Inicializado com Sucesso!");
+  }
+} );
